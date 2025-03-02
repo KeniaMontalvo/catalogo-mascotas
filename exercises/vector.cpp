439c0767656cdf8746215d855b86f145e0408bd4
Kenia Montalvo <kenlimonra@gmail.com>
2025-03-02 10:50:37 -0600

    creación de un vector

---
+++ exercises/vector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() //Inicio del programa
+{
+    //Declarar un vector
+    int vector [2];
+    
+    //Declarar los valores
+    vector[0] = 12;
+    vector[1] = 15;
+    vector[2] = 38;
+    
+    //Mostrar los valores del vector
+    printf("Vector en la posición 0 es: %d\n", vector[0]);
+    printf("Vector en la posición 1 es: %d\n", vector[1]);
+    printf("Vector en la posición 2 es: %d\n", vector[2]);
+    
+    return 0;
+}
